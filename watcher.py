54893596deafe4c6778e1bca4bd39dc33360146c
vijaymach <53861359+vijaymach@users.noreply.github.com>
2025-02-23 03:20:36 -0500

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7262217536:AAHX6ROfNRk1ZH9ZcwokcwvyR4jFtk3fKP8'
-ADMIN_ID = '1065510487'
+API_TOKEN = '7682977062:AAHLfgmu6YPt1-ag-Fbg29E8fz-2Asn-RbA'
+ADMIN_ID = '5253612194'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
